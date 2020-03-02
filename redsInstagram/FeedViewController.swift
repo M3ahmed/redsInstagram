717ca5c8f505ef4798d91d26c18944fffae39ab9
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-03-01 19:17:43 -0800

    having an issue with constraints again

--- redsInstagram/FeedViewController.swift
+++ redsInstagram/FeedViewController.swift
@@ -77,12 +77,34 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
+    
     @IBAction func onLogoutButton(_ sender: Any) {
         
         let main = UIStoryboard(name: "Main", bundle: nil)
         let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
         let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
         sceneDelegate.window?.rootViewController = loginViewController
+        
+        PFUser.logOut()
     }
     
     
